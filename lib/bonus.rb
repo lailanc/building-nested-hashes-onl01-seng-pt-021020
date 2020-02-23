8849f2fb9b7781938b0c75e1650ce1ed1c99ef86
Laila Campbell <lailacampbell96@gmail.com>
2020-02-23 23:53:13 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,10 +21,8 @@ def bonus
   }
 
   #code your solution here:
+  epic_tragedy["Jon Snow"][:favorite_ice_cream_flavors]
   
-
-  
-
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
